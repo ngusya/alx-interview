f3fa11b44cfa920f9670f2f4ec1883631e214dff
Carthage <carthage@ubuntu.myguest.virtualbox.org>
2024-04-29 01:06:24 +0300

    Fixed pycodestyle validation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""returns a list of lists of integers representing the Pascal’s triangle of n"""
+"""Pascal’s triangle interview challenge"""
 
 
 def pascal_triangle(n):
-    """returns a list of lists of numbers
-    representing the pascal triangle"""
+    """returns a list of lists of numbers representing the pascal triangle"""
     if n <= 0:
         return []
 
